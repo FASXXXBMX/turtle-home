61649c7d6bbac3400b2554fbd623bccbb2cc41bb
Egor <danil.ch.2001.08@gmail.com>
2023-07-13 16:32:12 +0300

    212

---
+++ shapes.py
@@ -0,0 +1,10 @@
+
+
+def drawSun(t,  x, y, size):
+    t.setheading(0)#поворот напровлении верх
+    t.fillcolor("yellow")
+    t.penup()#ручкаверх
+    t.goto(x,y)#переход x y
+    t.pendown()#ручкавнис
+    t.begin_fill()#рисовать внутриности крга
+    t.
